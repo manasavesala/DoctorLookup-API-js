a9f9f13cbe142a19e37f2bdd799229402a3a7f06
Manasa Vesala <vesalamanasa@gmail.com>
2019-01-18 12:00:24 -0800

    Added search Doctor by issue

--- src/main.js
+++ src/main.js
@@ -16,16 +16,18 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       console.log(body);
 
-      $('.doctorsList').text(`Doctors named "<h4>${searchDoctor}</h4>" in your area :`);
+      $('.doctorsList').text(`Doctors named "${searchDoctor}" in your area :`);
       if(body.data.length === 0){
         $('.doctorsList').text(`Sorry, there are no results for your search.`)
       } else {
         for (let i = 0; i < body.data.length; i++) {
-          $('.doctorsList').append(`<h2><br>${body.data[i].profile.first_name} `+`${body.data[i].profile.last_name}`+`<br></h2><br><h4>Gender: </h4> ${body.data[i].profile.gender}`+` <br> <h4>Phone number:</h4>
+          $('.doctorsList').append(`<h2><br>${body.data[i].profile.first_name} `+`
+            ${body.data[i].profile.last_name}`+`<br></h2><br><h4>Gender: </h4> 
+            ${body.data[i].profile.gender}`+` <br> <h4>Phone number:</h4>
             ${body.data[i].practices[0].phones[0].number}<br> <h4> Address: </h4>`+`
             ${body.data[i].practices[0].visit_address.street}<br>`+`
             ${body.data[i].practices[0].visit_address.city}<br>`+`<h4>Accepting New Patients:</h4> `+` 
-            ${body.data[i].practices[0].accepts_new_patients}`);
+            ${body.data[i].practices[0].accepts_new_patients}`+`<hr>`);
           }
         }
       }, function (error) {
@@ -33,5 +35,37 @@ $(document).ready(function() {
 
       });
   });
+
+  $('#issueForm').submit(function(event) {
+    event.preventDefault();
+    let searchIssue = $('#issue').val();
+    let docsList = new Doctor();
+    let promise = docsList.getDoctorByIssue(searchIssue);
+
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      console.log(body);
+
+      $('.doctorsList').text(`Doctors who can help with "${searchIssue}":`);
+      if(body.data.length === 0){
+        $('.doctorsList').text(`Sorry, there are no results for your search.`)
+      } else {
+        for (let i = 0; i < body.data.length; i++) {
+          $('.doctorsList').append(`<h2><br>${body.data[i].profile.first_name} `+`
+            ${body.data[i].profile.last_name}`+`<br></h2><br><h4>Gender: </h4> 
+            ${body.data[i].profile.gender}`+` <br> <h4>Phone number:</h4>
+            ${body.data[i].practices[0].phones[0].number}<br> <h4> Address: </h4>`+`
+            ${body.data[i].practices[0].visit_address.street}<br>`+`
+            ${body.data[i].practices[0].visit_address.city}<br>`+`</h4> <br>`+` <h4>Accepting New Patients:</h4> `+`
+            ${body.data[i].practices[0].accepts_new_patients}`+`<br>`+`<h4>description</h4> `+`
+            ${body.data[i].specialties[0].description}`+`<br><h4>Title: </h4>`+`
+            ${body.data[i].specialties[0].actor}`+`<hr>`);
+          }
+        }
+      }, function (error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+
+      });
+    });
 
 });
